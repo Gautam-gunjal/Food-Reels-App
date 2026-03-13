8158fa967faf028a49554599b94854587c47868f
Gautam Gunjalgit config --global user.email <gautam.2003.gunjal@gmail.com>
2026-03-13 18:49:23 +0530

    added port in env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const app = require("./src/app")
 const db = require('./src/db/db')
-
+const PORT = process.env.PORT || 3000
 db()
 
-app.listen(3000,()=>{
-    console.log('server running on port 3000');
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
